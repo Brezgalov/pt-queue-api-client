92821ae35fd82ede226fd1e67807c560e4d7cffc
Oleg Brezgalov <brezgalov.developer@gmail.com>
2022-11-01 13:11:39 +0300

    timeslot collection array access

--- src/ResponseAdapters/TimeslotsCollection.php
+++ src/ResponseAdapters/TimeslotsCollection.php
@@ -9,7 +9,7 @@ use Brezgalov\QueueApiClient\ResponseAdapters\Dto\TimeslotDto;
  * Class TimeslotsCollection
  * @package Brezgalov\QueueApiClient\ResponseAdapters
  */
-class TimeslotsCollection extends BaseResponseAdapter implements \Iterator
+class TimeslotsCollection extends BaseResponseAdapter implements \Iterator, \ArrayAccess
 {
     /**
      * @var int
@@ -58,5 +58,63 @@ class TimeslotsCollection extends BaseResponseAdapter implements \Iterator
     public function valid()
     {
         return is_array($this->responseData) && array_key_exists($this->position, $this->responseData);
+    }
+
+    /**
+     * Whether a offset exists
+     * @link https://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return bool true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists($offset)
+    {
+        return $this->getIsOk() && array_key_exists($offset, $this->responseData);
+    }
+
+    /**
+     * Offset to retrieve
+     * @link https://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        return $this->getIsOk() ? $this->responseData[$offset] : null;
+    }
+
+    /**
+     * Offset to set
+     * @link https://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        throw new \Exception(self::class . " is a ReadOnly");
+    }
+
+    /**
+     * Offset to unset
+     * @link https://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        throw new \Exception(self::class . " is a ReadOnly");
     }
 }
